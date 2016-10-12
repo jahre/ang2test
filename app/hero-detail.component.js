2a5c3a3c7c502ac380a82dff788ec10b7193117a
jahre <jahre@tut.by>
2016-10-12 07:57:51 +0300

    routing mid-work

--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -9,18 +9,22 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var AppComponent = (function () {
-    function AppComponent() {
-        this.title = 'Tour of Heroes';
+var hero_1 = require('./hero');
+var HeroDetailComponent = (function () {
+    function HeroDetailComponent() {
     }
-    AppComponent = __decorate([
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', hero_1.Hero)
+    ], HeroDetailComponent.prototype, "hero", void 0);
+    HeroDetailComponent = __decorate([
         core_1.Component({
-            selector: 'my-app',
-            template: "\n    <h1>{{title}}</h1>\n    <nav>\n      <a routerLink=\"/dashboard\">Dashboard</a>\n      <a routerLink=\"/heroes\">Heroes</a>\n    </nav>\n    <router-outlet></router-outlet>\n  "
+            selector: 'my-hero-detail',
+            template: "\n\t  <div *ngIf=\"hero\">\n\t  \t<h2>{{hero.name}} details!</h2>\n\t  \t<div><label>id: </label>{{hero.id}}</div>\n\t  \t<div>\n\t    \t<label>name: </label>\n\t    \t<input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n\t  \t</div>\n\t  </div>\n  \t"
         }), 
         __metadata('design:paramtypes', [])
-    ], AppComponent);
-    return AppComponent;
+    ], HeroDetailComponent);
+    return HeroDetailComponent;
 }());
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+exports.HeroDetailComponent = HeroDetailComponent;
+//# sourceMappingURL=hero-detail.component.js.map
